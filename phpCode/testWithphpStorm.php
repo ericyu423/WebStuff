c044a57b974632a9a6c13b706b1fc5a9e4663a8f
ericyu423 <ericyu423@hotmail.com>
2017-10-20 23:20:26 -0700

    Add files via upload

---
+++ phpCode/testWithphpStorm.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ericyu423
+ * Date: 10/18/17
+ * Time: 7:51 PM
+ */
+
+
+
+
+$x = "hello my name is eric";
+
+
+if (strpos($x,'eric')!= false) {
+    echo 'name found';
+}else {
+    echo 'no name is found';
+}
+
+
+?>
